057383349597e16b3f0e4f2b27aed70f521c227c
bobaso <yoshioka.mwork@gmail.com>
2026-09-07 15:14:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -298,8 +298,6 @@ let loseEnabled = true;
 let loseProbability = 0;
 let loseStock = 0;
 let instagramRetryEnabled = false;
-let instagramRetryUrl =
-    "https://www.instagram.com/umakitack/";
 /* =========================================
    メッセージ
 ========================================= */
